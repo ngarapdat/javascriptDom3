7a4a5e032c8e0adce5c8fe4f3074a3b9a57d6ecf
ngarapdatmanik <ngarapdatmanik@gmail.com>
2023-03-30 23:02:03 +0700

    menambahkan accordian,filter,dan link active

--- main.js
+++ main.js
@@ -17,3 +17,46 @@ function checkBoxes() {
   };
 }
 
+// aacordian
+const headerItems = document.querySelectorAll(".question");
+headerItems.forEach(headerItem => {
+  headerItem.addEventListener("click", ()=> {
+    headerItem.classList.toggle("active");
+    const bodyItem = headerItem.nextElementSibling;
+    if (headerItem.classList.contains('active')) {
+      bodyItem.style.maxHeight = bodyItem.scrollHeight + "px";
+    } else {
+      bodyItem.style.maxHeight = 0;
+    }
+  });
+});
+
+//active
+const menuItems = document.querySelectorAll(".header-list a");
+
+menuItems.forEach(item => {
+  if (item.href === window.location.href) {
+    item.classList.add("active");
+  }
+});
+//filter
+// mengambil nilai parameter query "filter"
+const urlParams = new URLSearchParams(window.location.search);
+const filterValue = urlParams.get('filter');
+
+// mencari elemen dengan class "minuman" dan mencocokkan id dengan nilai filter
+const minumanElems = document.querySelectorAll('.minuman');
+minumanElems.forEach(elem => {
+  if (elem.id === filterValue) {
+    elem.style.display = 'block';
+  } else {
+    elem.style.display = 'none';
+  }
+});
+if (!filterValue) {
+  minumanElems.forEach(elem => {
+    elem.style.display = 'block';
+  });
+}
+
+
